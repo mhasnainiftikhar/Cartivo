61f1088013ea3e8177039ae196d4226a98b6fd29
Muhammad Hasnain Iftikhar <160528829+mhasnainiftikhar@users.noreply.github.com>
2026-01-26 13:08:10 +0500

    Create folder structure,Implement Roles and Seller Model

---
+++ backend/src/model/seller.js
@@ -0,0 +1,81 @@
+import mongoose from "mongoose";
+import UserRoles from "../domain/UserRole";
+
+const sellerSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    mobile:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+        select:false
+    },
+    businessDetails:{
+        businessName:{
+            type:String,
+        },
+        businessEmail:{
+            type:String,
+        },
+        businessMobile:{
+            type:String,
+        },
+        businessAddress:{
+            type:String,
+        }
+    },
+    bankDetails:{
+        accountHolderName:{
+            type:String,
+        },
+        accountNumber:{
+            type:String,
+        },
+        ifscCode:{
+            type:String,
+        },
+        bankName:{
+            type:String,
+        }
+    },
+    picupAddress:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Address'
+    },
+    GST:{
+        type:String,
+        required:true,   
+    },
+    role:{
+        type:String,
+        enum:[UserRoles.SELLER],
+        default:UserRoles.SELLER
+    },
+    accountStatus:{
+        type:String,
+        enum:[AccountStatus.PENDING_VERIFICATION,
+              AccountStatus.ACTIVE,
+              AccountStatus.SUSPENDED,
+              AccountStatus.DEACTIVATED,
+              AccountStatus.BANNED,
+              AccountStatus.CLOSED],
+        default:AccountStatus.PENDING_VERIFICATION
+    }
+},{
+    timestamps:true
+
+});
+
+const Seller=mongoose.model('Seller',sellerSchema);
+export default Seller;
